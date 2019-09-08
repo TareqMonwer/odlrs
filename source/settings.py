c0bfadb5a3602815c1f255dcd5eaaab58348259c
TareqMonwer <tareqmonwer137@gmail.com>
2019-09-08 15:24:47 +0600

    config

--- source/settings.py
+++ source/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'diagnostic_centers',
     'tests',
     'report_processing',
+    'whitenoise.runserver_nostatic',
 
     # allauth
     'allauth',
@@ -63,6 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,12 +149,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
