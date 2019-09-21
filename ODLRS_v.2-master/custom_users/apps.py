e0def66dd5421d30c4efa53b0d49642e0588c164
TareqMonwer <tareqmonwer137@gmail.com>
2019-09-21 21:16:40 +0600

    new updates for responsive design

--- ODLRS_v.2-master/custom_users/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomUsersConfig(AppConfig):
-    name = 'custom_users'
-
-    def ready(self):
-        import custom_users.signals
--- ODLRS_v.2-master/report_processing/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportProcessingConfig(AppConfig):
-    name = 'report_processing'
